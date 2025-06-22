5dfc4c7bbc54039457c657e78849fc9c2048257b
Terminnus <terminnus95@gmail.com>
2025-06-21 18:47:36 -0600

    Create the third page
    This commit adds the «Finish page» this page shows the number previously
    inputted on the «loading page» and also contains a «quit» button
    
    This commit also made some other modifications like a toast warning the
    user when he tries to input a doesnt contain only numbers, and lots of
    comments

--- src/window.py
+++ src/window.py
@@ -24,41 +24,57 @@ import time, random, threading
 class FakeGuessingGameWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'FakeGuessingGameWindow'
 
-    master = Gtk.Template.Child()
+    ###########
+    # WIDGETS #
+    ###########
 
-    # Widgets from the «input_page»
+	# Universal widgets
+    master = Gtk.Template.Child()
+    toast_overlay = Gtk.Template.Child()
+
+    # Widgets from «input_page»
+    input_page = Gtk.Template.Child()
     number_entry = Gtk.Template.Child()
 
-    # Widgets from the «loading_page»
+    # Widgets from «loading_page»
     loading_page = Gtk.Template.Child()
     loading_message = Gtk.Template.Child()
 
-    # Widgets from the «finish_page»
+    # Widgets from «finish_page»
     finish_page = Gtk.Template.Child()
+    number_label = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        ###########
+        # THREADS #
+		###########
+
         #Inicialize «modify_loading_message()» on a separate thread
         modify_loading_message_thread = threading.Thread(target=self.modify_loading_message, daemon=True)
         modify_loading_message_thread.start()
-        #Inicialize «hange_loading_page_to_finish_page()» on a separate thread
+        #Inicialize «change_loading_page_to_finish_page()» on a separate thread
         change_loading_page_thread = threading.Thread(target=self.change_loading_page_to_finish_page, daemon=True)
         change_loading_page_thread.start()
 
         ###########
         # ACTIONS #
         ###########
+
         self.create_action("guess", self.on_guess)
 
     ######################
     # LOADING PAGE LOGIC #
     ######################
+
     def modify_loading_message(self, *kwargs):
         """
         This piece of code will modify loading_message's label every 1-2 seconds
         to a string on «messages»
         """
+
+        # This list contains all of the messages that the label could possibly get
         messages = ["Initiating Mind Reading Algorithm", "Analyzing Neural Patterns",
         "Decoding Cerebral Signals", "Identifying Neural Pathways",
         "Initiating Deep Scan", "Initiating Quantum Neural Inference",
@@ -71,52 +87,59 @@ class FakeGuessingGameWindow(Adw.ApplicationWindow):
                 print("changed!")
                 self.loading_message.set_label(random.choice(messages))
                 time.sleep(random.randint(2, 5)) #Wait some time before changing it again
+            #Break the loop when the page becomes «finish_page»
+            elif self.master.get_visible_page().get_tag() == "finish_page":
+                break
 
     def change_loading_page_to_finish_page(self, *kwargs):
         """
         This function will change the page from «loading_page» to «finish_page»
-        after some time
+        after 10-25 seconds after the page becomes «loading_page»
         """
         while True:
             if self.master.get_visible_page().get_tag() == "loading_page":
-                time.sleep(random.randint(10, 25))
+                time.sleep(random.randint(10, 25)) # Wait before changing
                 print("change page!")
-                self.master.push(self.finish_page)
+                self.master.push(self.finish_page) # Changing the page
                 break
-
 
     ###################
     # ACTION FUNCIONS #
     ###################
+
     def on_guess(self, *args):
         """
         This funcion is the effect of presing the «guess» button on the
         first page, this will do the following:
-        *Check if the string inputted only contains numbers
-        if not, show a banner teling the user to ajust their input.
-        *if the string just contains numbers, go over to the second page
+            *Check if the string inputted only contains numbers
+            if not, show a toast teling the user to ajust their input.
+            *if the string just contains numbers, go over to the next page
         """
 
         guess = self.number_entry.get_text()
 
-        #Try to convert the guess into a int to check if the guess is a pure integer
         try:
+        	#Try to convert the guess into a int to check if the guess only contains numbers
             int(guess)
+
             print("valid string")
 
-            #If all is correct, move over to the next page
+			#If it is an int, move over to the next page and make the text in «number_label» into «guess»
             self.master.push(self.loading_page)
-            #print(self.master.get_visible_page().get_tag())
+            self.number_label.set_label(guess)
 
-        except Exception as e:
-        #If not, show a banner warning the user [NOT CURRENTLY WORKING]
-            print(f"invalid sring{e}")
-            banner = Adw.Banner(title="Invalid value",
-            button_label="ok")
-            banner.set_revealed(True)
+        except ValueError:
+        	print("invalid sring")
 
+        #If not, show a toast
+        	toast = Adw.Toast(title="Only Numbers are Accepted", timeout=2)
+        	self.toast_overlay.add_toast(toast)
 
         print(guess)
+
+	########
+	# MISC #
+	########
 
     def create_action(self, name, callback, shortcuts=None):
         """Add an application action.
@@ -132,3 +155,4 @@ class FakeGuessingGameWindow(Adw.ApplicationWindow):
         self.add_action(action)
         if shortcuts:
             self.set_accels_for_action(f"app.{name}", shortcuts)
+
